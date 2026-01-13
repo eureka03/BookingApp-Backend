912ad6322835177d893102b1dacad0ea19cf1264
Eureka Baloyi <eurekab@vertice.software>
2026-01-13 22:39:04 +0200

    entity modifications

---
+++ src/main/java/bookingApp/repository/BookingRepository.java
@@ -0,0 +1,7 @@
+package bookingApp.repository;
+
+import bookingApp.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRepository extends JpaRepository<Booking, Integer> {
+}
---
+++ src/main/java/bookingApp/repository/ReviewRepository.java
@@ -0,0 +1,7 @@
+package bookingApp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import bookingApp.entity.Rev
+
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+}
